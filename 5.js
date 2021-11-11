8d0852313850f66651745112fa09f696fd90c8aa
escuela <a316099@uach.mx>
2021-11-10 22:12:37 -0700

    Problema 5

---
+++ 5.js
@@ -0,0 +1,11 @@
+let contador = 0;
+for (let x = 1; x < 101; x++ ){
+    if (x % 2 != 0){
+        console.log(x);
+        contador ++;
+    }
+}
+console.log("La cantidad de numeros impares es:")
+console.log(contador)
+
+
