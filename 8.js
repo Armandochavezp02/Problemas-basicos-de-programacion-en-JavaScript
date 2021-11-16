522433fa8926bf9ffbd20de10d60f706495a35e0
escuela <a316099@uach.mx>
2021-11-15 21:09:45 -0700

    Problema 8

---
+++ 8.js
@@ -0,0 +1,12 @@
+const array = [];
+
+while(x == true){
+    let input = prompt("Introduzca una letra");
+
+    if(input == 'N'){
+        array.push(input)
+    }
+    else if(input == 'S'){
+        array.push(input)
+    }
+}
