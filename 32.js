68eb6df2d7beac52e811edef5787f83efb189fb2
escuela <a316099@uach.mx>
2021-11-15 17:55:24 -0700

    Problema 32

---
+++ 32.js
@@ -0,0 +1,6 @@
+const nombres = ["Ruben", "Claudia", "Julieta", "Alan", "Yadira", "Solomeo", "Maximiliano", "Euclidiano", "Herculano", "Alvaro","Benito","Ramon", "Liza", "Cesar", "Humberto", "Claudio", "Esperanza", "Popeye", "Shawn", "Micheals"];
+nombres.forEach(xfunc);
+
+function xfunc(y) {
+    console.log(y)
+}
